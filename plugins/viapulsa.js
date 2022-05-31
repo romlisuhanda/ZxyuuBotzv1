826cd6f4d0852420aa5643fba1be3fba5cd7808d
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-31 10:20:19 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -1,11 +1,11 @@
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Dana
-╠➥ *6282361160044*
+╠═ Untuk Pembayaran Via pulsa
+╠➥ ---
 ╠➥ Chat Owner:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+║- wa.me/6281224804625
+╠═〘 *ZxyuuBotz* 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 

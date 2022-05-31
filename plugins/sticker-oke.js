c00fb49f4649efc7b22b9570463f65b5a5064f58
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-31 12:02:44 +0700

    Update sticker-oke.js

--- plugins/sticker-oke.js
+++ plugins/sticker-oke.js
@@ -4,7 +4,7 @@ let stc = fs.readFileSync('./sticker/Oke.webp')
 conn.fakeReply(m.chat, stc, '0@s.whatsapp.net', '*OKE*', 'status@broadcast')
 }
 
-handler.customPrefix = /^(iya|ok|oke|okey)$/i
+handler.customPrefix = /^(iya|ok|oke|siap)$/i
 handler.command = new RegExp
 
 module.exports = handler

143f3b1893c27b4593b26512c689df45075bf390
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-31 10:05:20 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn }) => {
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Klil Button Di Bawah
 ║
-╠═〘 Hyzer 〙 ═`.trim(), '❖ Kanao-Bot', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
+╠═〘 *Zxyuu* 〙 ═`.trim(), '❖ ZxyuuBotz', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
 }
 
 handler.command = /^sewabot$/i
